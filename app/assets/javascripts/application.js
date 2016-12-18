5fbfe7f73fcb6cdb32b5cc8c5919bc1938aec8f1
Michal Karpinski <karp0506@gmail.com>
2016-12-18 13:26:26 +0100

    Unarchive option added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,8 @@ document.addEventListener("turbolinks:load", function() {
   $('.archive').click(function() {
     Turbolinks.visit('/notifications/'+$(this).val()+'/archive'+location.search)
   });
+
+  $('.unarchive').click(function() {
+    Turbolinks.visit('/notifications/'+$(this).val()+'/unarchive'+location.search)
+  });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/notifications/:id/archive', to: 'notifications#archive'
+  get '/notifications/:id/unarchive', to: 'notifications#unarchive'
   get '/sync', to: 'notifications#sync'
   root to: 'notifications#index'
 end
