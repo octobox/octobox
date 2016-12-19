c0643ab91a1eac51a3418f8dcae041e95f93c09d
Mark Tareshawty <tarebyte@github.com>
2016-12-18 20:27:17 -0500

    set scope from env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,5 +3,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :github,
            Rails.application.secrets.github_client_id,
            Rails.application.secrets.github_client_secret,
-           scope: 'notifications'
+           scope: ENV['GITHUB_SCOPE']
 end
