669d98110708df7324da335d7bf0ba2b22ed6a54
Michal Karpinski <karp0506@gmail.com>
2016-12-19 10:57:10 +0100

    remove toggle-shortcuts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require js.cookie
 //= require_tree .
 
 document.addEventListener("turbolinks:load", function() {
@@ -16,39 +15,15 @@ document.addEventListener("turbolinks:load", function() {
     $.get('/notifications/'+$(this).data('id')+'/star')
   });
 
-  if(Cookies.get('shortcuts') == "true") {
-    $('.table-notifications tbody tr').first().find("td").first().addClass("current");
-  }
-  
-  // Keyboard Shortcuts
-  
-  toggle_shortcuts = function() {
-    enabled = Cookies.get('shortcuts');
-    if (enabled == "true") {
-      Cookies.set('shortcuts', false);
-      $('.table-notifications td').removeClass("current");
-    } else {
-      Cookies.set('shortcuts', true);
-      $('.table-notifications tbody tr').first().find("td").first().addClass("current");
-    }
-  }
-  
-  $('.toggle-shortcuts').click(function(e) {
-    e.preventDefault();
-    toggle_shortcuts();
-  });
+  // Set current notification for navigation
+  $('.table-notifications tbody tr').first().find("td").first().addClass("current");
 });
 
 // Add key events only once
 
 $( document ).ready(function() {
-  
-  if(Cookies.get('shortcuts') == undefined) {
-    Cookies.set('shortcuts', false);
-  }
-
   $(document).keydown(function(e) {
-    if ( e.which == 74 ) {
+    if ( e.which == 74 ) {  // j
       current = $('td.current');
       next = $(current).parent().next();
       if(next.length > 0) {
@@ -56,8 +31,7 @@ $( document ).ready(function() {
         $(next).find('td').first().addClass("current");
       }
     }
-
-    if ( e.which == 75 ) {
+    if ( e.which == 75 ) { // k
       current = $('td.current');
       prev = $(current).parent().prev();
       if(prev.length > 0) {
@@ -65,17 +39,11 @@ $( document ).ready(function() {
         $(prev).find('td').first().addClass("current");
       }
     }
-
-    if ( e.which == 83 ) {
-      if(Cookies.get('shortcuts') == "true") {
-        $('td.current').parent().find('.toggle-star').click();
-      }
+    if ( e.which == 83 ) { // s
+      $('td.current').parent().find('.toggle-star').click();
     }
-
-    if ( e.which == 65 ) {
-      if(Cookies.get('shortcuts') == "true") {
-        $('td.current').parent().find('.archive').click();
-      }
+    if ( e.which == 89 ) { // y
+      $('td.current').parent().find('.archive').click();
     }
   });
 });
