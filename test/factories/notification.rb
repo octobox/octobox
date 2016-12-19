29499b8316b6f944ef1de3a3cea330b2c86efc2c
pezholio <pezholio@gmail.com>
2016-12-19 15:24:04 +0000

    Add factory_girl

---
+++ test/factories/notification.rb
@@ -0,0 +1,16 @@
+FactoryGirl.define do
+  factory :notification do
+    repository_id 930405
+    repository_full_name "andrew/octobox"
+    subject_title "Test"
+    subject_url "https://api.github.com/repos/andrew/octobox/issues/123"
+    subject_type "Issue"
+    reason "subscribed"
+    unread true
+    updated_at "2015-06-22 14:37:57"
+    last_read_at "2015-06-22 13:34:48 UTC"
+    url "https://api.github.com/notifications/threads/930405"
+    archived false
+    starred false
+  end
+end
---
+++ test/factories/user.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user do
+    github_id 1234
+    access_token '54c543ffd45343fg5434fgg556ff553232gg57890v'
+    github_login 'andrew'
+  end
+end
