cbbbb1b86372d4b35d2a4dbc20dd57d7496c7894
Brian Nelson <brian@kickserv.com>
2016-12-19 09:19:11 -0500

    Update mine

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,12 @@ module GithubInbox
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
   end
 end
