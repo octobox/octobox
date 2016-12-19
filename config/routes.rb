c425468c3adb856970b7f421e27fe88ccda2933d
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-19 20:53:54 +0000

    Added logout link to menu, fixes #50

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
 
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 
