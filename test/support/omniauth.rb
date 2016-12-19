5dc3e0bca336891cd434b5608439f922e78bff82
Mark Tareshawty <tarebyte@github.com>
2016-12-18 20:41:28 -0500

    add login to Omniauth

--- test/support/omniauth.rb
+++ test/support/omniauth.rb
@@ -4,5 +4,6 @@ OmniAuth.config.test_mode = true
 OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(
   'provider'    => 'github',
   'uid'         => 42,
+  'info'        => { 'nickname' => 'douglas_adams' },
   'credentials' => { 'token' => SecureRandom.hex(20) }
 )
