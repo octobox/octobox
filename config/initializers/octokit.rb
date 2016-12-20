d518640d44e6af930daa60331e43b6a8052fab56
Mark Tareshawty <tarebyte@github.com>
2016-12-20 15:26:55 -0500

    make it easier to obtain the GitHub domain

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
-if (github_domain = ENV.fetch('GITHUB_DOMAIN', nil))
-  Octokit.configure do |c|
-    c.api_endpoint = "#{github_domain}/api/v3"
-    c.web_endpoint = github_domain
-  end
+Octokit.configure do |c|
+  c.api_endpoint = Octobox.github_api_prefix
+  c.web_endpoint = Octobox.github_domain
 end
