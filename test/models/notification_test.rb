826e95d19e7800673cf56c3a5badc3711b5d18dd
Mark Tareshawty <tarebyte@github.com>
2016-12-19 20:07:54 -0500

    push failing test

--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,15 +1,8 @@
+# frozen_string_literal: true
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  setup do
-    notifications_url = %r{https://api.github.com/notifications}
-
-    body     = file_fixture('notifications.json')
-    headers  = { 'Content-Type' => 'application/json' }
-    response = { status: 200, body: body, headers: headers }
-
-    stub_request(:get, notifications_url).to_return(response)
-  end
+  setup { stub_notifications_request }
 
   test '#download fetches one months notification when a user has not been synched before' do
     travel_to "2016-12-19T19:00:00Z" do
@@ -29,5 +22,28 @@ class NotificationTest < ActiveSupport::TestCase
     Notification.download(user)
 
     assert_requested :get, "https://api.github.com/notifications?all=true&per_page=100&since=2016-12-19T19:00:00Z"
+  end
+
+  test "#download will" do
+    stub_notifications_request(body: file_fixture('repository_invitation_notification.json'))
+    user = users(:andrew)
+
+    Notification.download(user)
+
+    assert notification = Notification.last
+    assert_equal 'RepositoryInvitation', notification.subject_type
+    assert_match %r{https://github.com/.+/invitations$}, notification.subject_url
+  end
+
+  private
+
+  def stub_notifications_request(body: nil)
+    notifications_url = %r{https://api.github.com/notifications}
+
+    body     ||= file_fixture('notifications.json')
+    headers  = { 'Content-Type' => 'application/json' }
+    response = { status: 200, body: body, headers: headers }
+
+    stub_request(:get, notifications_url).to_return(response)
   end
 end
