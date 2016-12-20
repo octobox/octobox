5f0746c80dce4abfa9d99627fc67995e6d686d44
Mark Tareshawty <tarebyte@github.com>
2016-12-20 09:35:26 -0500

    add closable flash messages in layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,18 @@
 module ApplicationHelper
+  def bootstrap_class_for(flash_type)
+    { success: 'alert-success', error: 'alert-danger', alert: 'alert-warning', notice: 'alert-info' }[flash_type.to_sym] || flash_type.to_s
+  end
+
+  def flash_messages
+    flash.each do |msg_type, message|
+      concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in") do
+        concat content_tag(:button, 'x', class: 'close', data: { dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
+
   def notification_icon(subject_type)
     case subject_type
     when 'RepositoryInvitation'
