760b12c945a22a0363caf407b9469e26d5b4df70
michaelrauh <michaelrauh@ymail.com>
2016-12-20 19:25:05 -0500

    lowered complexity of notification_icon in application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,34 +14,16 @@ module ApplicationHelper
   end
 
   def notification_icon(subject_type)
-    case subject_type
-    when 'RepositoryInvitation'
-      'mail-read'
-    when 'Issue'
-      'issue-opened'
-    when 'PullRequest'
-      'git-pull-request'
-    when 'Commit'
-      'git-commit'
-    end
+    subject_types = {'RepositoryInvitation' => 'mail-read', 'Issue' => 'issue-opened',
+      'PullRequest' => 'git-pull-request', 'Commit' => 'git-commit'}
+
+    subject_types[subject_type]
   end
 
   def reason_label(reason)
-    case reason
-    when 'comment'
-      'primary'
-    when 'author'
-      'success'
-    when 'state_change'
-      'info'
-    when 'mention'
-      'warning'
-    when 'assign'
-      'danger'
-    when 'subscribed'
-      'default'
-    else
-      'default'
-    end
+    reasons = {'comment' => 'primary', 'author' => 'success', 'state_change' => 'info', 'mention' => 'warning',
+       'assign' => 'danger' }
+    reasons.default = 'default'
+    reasons[reason]
   end
 end
