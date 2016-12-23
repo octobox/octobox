77e73936395d0eece1bb90617fcf4dab5f9c2244
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-23 11:56:45 +0000

    Move some filter logic into helper methods

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -2,4 +2,19 @@ module NotificationsHelper
   def menu_separator(custom_class=nil)
     "<li class='divider #{custom_class}'></li>".html_safe
   end
+
+  def filters
+    {
+      reason:  params[:reason],
+      status:  params[:status],
+      repo:    params[:repo],
+      type:    params[:type],
+      archive: params[:archive],
+      starred: params[:starred]
+    }
+  end
+
+  def any_active_filters?
+    filters[:reason].present? || filters[:status].present? || filters[:repo].present? || filters[:type].present?
+  end
 end
