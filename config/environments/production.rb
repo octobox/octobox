f8789c2d183170d54d38f365e8e39a6b52a5bb7b
Andrew Nesbitt <andrewnez@gmail.com>
2016-12-23 10:57:54 +0000

    Only enable memcached if ENV var is set

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,22 +50,24 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  memcachedcloud_servers = ENV['MEMCACHEDCLOUD_SERVERS'].split(',')
+  memcachedcloud_servers = ENV.fetch('MEMCACHEDCLOUD_SERVERS', '').split(',')
+  if memcachedcloud_servers.any?
 
-  dalli_store_name_and_password = {
-    username: ENV['MEMCACHEDCLOUD_USERNAME'],
-    password: ENV['MEMCACHEDCLOUD_PASSWORD']
-  }
+    dalli_store_name_and_password = {
+      username: ENV['MEMCACHEDCLOUD_USERNAME'],
+      password: ENV['MEMCACHEDCLOUD_PASSWORD']
+    }
 
-  dalli_store_config = {
-    namespace:  'OCTOBOX',
-    expires_in: (ENV['REQUEST_CACHE_TIMEOUT'] || 30).to_i.minutes,
-    pool_size:  5
-  }
+    dalli_store_config = {
+      namespace:  'OCTOBOX',
+      expires_in: (ENV['REQUEST_CACHE_TIMEOUT'] || 30).to_i.minutes,
+      pool_size:  5
+    }
 
-  config.cache_store = :dalli_store,
-                       memcachedcloud_servers,
-                       dalli_store_name_and_password.merge(dalli_store_config)
+    config.cache_store = :dalli_store,
+                         memcachedcloud_servers,
+                         dalli_store_name_and_password.merge(dalli_store_config)
+  end
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
