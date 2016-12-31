0933f1e16dc4026221f7b3e9eae4a65ace3acf8f
sam <samjacobclift@gmail.com>
2016-12-31 16:01:47 +0000

    remove migration

--- db/migrate/20161231115932_add_full_text_search_indexes.rb
+++
@@ -1,9 +0,0 @@
-class AddFullTextSearchIndexes < ActiveRecord::Migration[5.0]
-  def up
-    execute "create index notifications_subject_title on notifications using gin(to_tsvector('english', subject_title))"
-  end
-
-  def down
-    execute "drop index notifications_subject_title"
-  end
-end
