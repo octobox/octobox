aebe6d89fd85a58bb0697cfa8d56c110b5c41d50
Will Roden <willabides@github.com>
2017-01-22 11:35:34 -0600

    Test copyright_message and license_message

--- test/helpers/application_helper_test.rb
+++ test/helpers/application_helper_test.rb
@@ -1,3 +1,5 @@
+require 'test_helper'
+
 class ApplicationHelperTest < ActionView::TestCase
   test "should return the correct bootstrap class for each type of flash" do
     assert_equal bootstrap_class_for(:success), 'alert-success'
@@ -5,5 +7,27 @@ class ApplicationHelperTest < ActionView::TestCase
     assert_equal bootstrap_class_for(:alert), 'alert-warning'
     assert_equal bootstrap_class_for(:notice), 'alert-info'
     assert_equal bootstrap_class_for(:foobar), 'foobar'
+  end
+
+  test 'copyright_message has no et al link when no contributors are found' do
+    Octobox.stubs(:contributors).returns(nil)
+    assert_equal '© 2017 Andrew Nesbitt, et al', copyright_message
+  end
+
+  test 'copyright_message has et al link' do
+    stub_contributors
+    assert_equal "© 2017 Andrew Nesbitt, <a href='#' data-toggle='modal' data-target='#et-al'>et al</a>", copyright_message
+  end
+
+  test 'license_message has correct link when SOURCE_REPO is not set' do
+    ENV.stubs(:[]).with('SOURCE_REPO').returns(nil)
+    expected_message = "<a href='https://github.com/octobox/octobox'>Source</a> available under <a href='https://github.com/octobox/octobox/blob/master/LICENSE.txt'>AGPL 3.0</a>"
+    assert_equal expected_message, license_message
+  end
+
+  test 'license_message has correct link when SOURCE_REPO is set' do
+    ENV.stubs(:[]).with('SOURCE_REPO').returns('https://github.com/foo/bar')
+    expected_message = "<a href='https://github.com/foo/bar'>Source</a> available under <a href='https://github.com/foo/bar/blob/master/LICENSE.txt'>AGPL 3.0</a>"
+    assert_equal expected_message, license_message
   end
 end
