731747bceb733527919e94dbf00154168e715850
Andrew Nesbitt <andrewnez@gmail.com>
2017-08-17 11:15:49 +0100

    Subjects can have many notifications

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,6 @@
 class Subject < ApplicationRecord
+  has_many :notifications, foreign_key: :subject_url, primary_key: :url
+
   def author_url
     "#{Octobox.config.github_domain}/#{author}"
   end
