4eb875f4bb0dea5ba28a6bf9368a5cd460d43d77
Andrew Nesbitt <andrewnez@gmail.com>
2017-08-17 20:22:14 +0100

    Changed loading of lib/octobox to fix test coverage

--- config/initializers/octokit.rb
+++ config/initializers/octokit.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+require Rails.root.join('lib/octobox')
+
 Octokit.configure do |c|
   c.api_endpoint = Octobox.config.github_api_prefix
   c.web_endpoint = Octobox.config.github_domain
