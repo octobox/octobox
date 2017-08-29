6491966e63f94292edf41814946f5851a051e9e9
Chris Arcand <chris@chrisarcand.com>
2017-08-29 08:58:30 -0500

    Add latest_comment_url to notifications

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170812203351) do
+ActiveRecord::Schema.define(version: 20170829135808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170812203351) do
     t.datetime "created_at", null: false
     t.boolean "starred", default: false
     t.string "repository_owner_name", default: ""
+    t.string "latest_comment_url"
     t.index "to_tsvector('english'::regconfig, (subject_title)::text)", name: "notifications_subject_title", using: :gin
     t.index ["user_id", "archived", "updated_at"], name: "index_notifications_on_user_id_and_archived_and_updated_at"
     t.index ["user_id", "github_id"], name: "index_notifications_on_user_id_and_github_id", unique: true
