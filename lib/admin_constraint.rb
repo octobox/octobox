9b0a138c1004be4a415345e4f8d3f36acc749403
Mark Tareshawty <tarebyte@github.com>
2018-02-19 12:17:49 -0500

    Fill in implementation for AdminConstraint#matches?

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -2,6 +2,12 @@
 
 class AdminConstraint
   def matches?(request)
+    user_id = request.cookie_jar.signed[:user_id]
+
+    if (user = User.find_by(id: user_id))
+      return user.admin?
+    end
+
     false
   end
 end
