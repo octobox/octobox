40ce8ed6ebccbce34f964dd1998d3b88952470af
Mark Tareshawty <tarebyte@github.com>
2018-02-19 11:33:26 -0500

    Add Sidekiq helpers and configuration for the test suite

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -21,6 +21,7 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include FactoryBot::Syntax::Methods
+  include SidekiqMinitestSupport
   include StubHelper
 end
 
