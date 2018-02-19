0ea46c7835455b8bb392d7253d7a7caaf1f997ae
Mark Tareshawty <tarebyte@github.com>
2018-02-19 11:36:01 -0500

    Add body to SyncNotificationsWorker

--- app/workers/sync_notifications_worker.rb
+++ app/workers/sync_notifications_worker.rb
@@ -5,5 +5,21 @@ class SyncNotificationsWorker
   sidekiq_options queue: :sync_notifications, unique: :until_executed
 
   def perform(user_id)
+    user = User.find_by(id: user_id)
+    return unless user.present?
+
+    user.try(:sync_notifications)
+  rescue Octokit::Unauthorized, Octokit::Forbidden => exception
+    handle_exception(exception, user)
+  rescue Octokit::BadGateway, Octokit::ServiceUnavailable => exception
+    handle_exception(exception, user)
+  rescue Faraday::ClientError => exception
+    handle_exception(exception, user)
+  end
+
+  private
+
+  def handle_exception(exception, user)
+    logger.error("[ERROR] SyncNotificationsJob#perform #{user.github_login} - #{exception.class}: #{exception.message}")
   end
 end
