6a4630c1394c12cf9671ab54ece5994383883bc3
jules2689 <julian@jnadeau.ca>
2018-04-04 17:58:01 -0400

    Return nil if remote subject cannot be found

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -122,6 +122,7 @@ class Notification < ApplicationRecord
     user.github_client.get(subject_url)
   rescue Octokit::Forbidden, Octokit::NotFound => e
     Rails.logger.warn("\n\n\033[32m[#{Time.now}] WARNING -- #{e.message}\033[0m\n\n")
+    nil
   end
 
   def update_subject
