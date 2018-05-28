e905e4ef2ad76e591f5a43026d740a56511187a8
Chase Southard <chase.southard@gmail.com>
2018-05-28 19:52:48 -0400

    move scheduler config into the server config block
    this addresses the problem where you might encounter
    an `invalid uri scheme ''` error as sidekiq doesn't
    seem to know about the redis_url

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,13 +4,14 @@ require 'sidekiq-scheduler'
 
 Sidekiq.configure_server do |config|
   config.redis = { url: Octobox.config.redis_url }
+  if Octobox.config.sidekiq_schedule_enabled?
+    config.on(:startup) do
+      Sidekiq.schedule = YAML.load_file(Octobox.config.sidekiq_schedule_path)
+      Sidekiq::Scheduler.reload_schedule!
+    end
+  end
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: Octobox.config.redis_url }
-end
-
-if Octobox.config.sidekiq_schedule_enabled?
-  Sidekiq.schedule = YAML.load_file(Octobox.config.sidekiq_schedule_path)
-  Sidekiq::Scheduler.reload_schedule!
 end
