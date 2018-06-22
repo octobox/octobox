1245e20873ecb9989518ffbb0d13e82a9eb6065f
Andrew Nesbitt <andrewnez@gmail.com>
2018-06-22 12:13:03 +0100

    Link latest_git_sha to the remote source_repo

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,7 +38,7 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test 'assigns latest git sha on settings page' do
     sign_in_as(@user)
     get settings_path
-    assert_equal assigns(:latest_git_sha).length, 7
+    assert_equal assigns(:latest_git_sha).length, 40
   end
 
   test 'updates api_token' do
