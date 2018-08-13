b05415cf00a396869c2e271c5297f8d97ddbbdc4
Andrew Nesbitt <andrewnez@gmail.com>
2018-08-13 15:03:14 +0100

    Add app_version to bugsnag config

--- config/initializers/bugsnag.rb
+++ config/initializers/bugsnag.rb
@@ -2,5 +2,6 @@ if Rails.application.secrets.bugsnag_api_key.present?
   Bugsnag.configure do |config|
     config.api_key = Rails.application.secrets.bugsnag_api_key
     config.release_stage = ENV["RAILS_ENV"]
+    config.app_version = ENV['HEROKU_RELEASE_VERSION']
   end
 end
