1ebb13ccda6bd70cad4bf0ae2caa1ab07625444a
Andrew Nesbitt <andrewnez@gmail.com>
2018-08-13 09:09:10 +0100

    Only query subject-dependent counts if fetch_subject is enabled

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -71,13 +71,17 @@ class NotificationsController < ApplicationController
   #
   def index
     scope = notifications_for_presentation
-    @states                = scope.distinct.joins(:subject).group('subjects.state').count
     @types                 = scope.distinct.group(:subject_type).count
     @unread_notifications  = scope.distinct.group(:unread).count
     @reasons               = scope.distinct.group(:reason).count
     @unread_repositories   = scope.distinct.group(:repository_full_name).count
-    @unlabelled            = scope.unlabelled.count
-    @bot_notifications     = scope.bot_author.count
+
+    if Octobox.config.fetch_subject
+      @states                = scope.distinct.joins(:subject).group('subjects.state').count
+      @unlabelled            = scope.unlabelled.count
+      @bot_notifications     = scope.bot_author.count
+    end
+
     scope = current_notifications(scope)
     check_out_of_bounds(scope)
 
