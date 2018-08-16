ae19b89aded61f7b9947775b7151266a4b41ebe9
Andrew Nesbitt <andrewnez@gmail.com>
2018-08-16 18:12:41 +0100

    Sync github_id onto subjects (#798)
    Will come in useful later when linking to certain parts of the issue/pr 
    page on GitHub

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_03_073302) do
+ActiveRecord::Schema.define(version: 2018_08_16_142404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2018_08_03_073302) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "html_url"
+    t.integer "github_id"
     t.index ["url"], name: "index_subjects_on_url"
   end
 
