1a0db2f5c1ea5eeb91dcb58766d03527e2f19f28
Andrew Nesbitt <andrewnez@gmail.com>
2018-08-19 20:13:37 +0100

    Enable pg_stat_statements postgres extension (#807)
    https://www.postgresql.org/docs/current/static/pgstatstatements.html

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_142404) do
+ActiveRecord::Schema.define(version: 2018_08_17_172203) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
 
   create_table "labels", force: :cascade do |t|
