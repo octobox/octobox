310d7996837bf93e1dfc6a6cbc5e5d51237cba6f
Andrew Nesbitt <andrewnez@gmail.com>
2018-08-22 14:36:07 +0100

    Simpler relationship between repositories and subjects

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,7 +1,10 @@
 class Repository < ApplicationRecord
   has_many :notifications, foreign_key: :repository_full_name, primary_key: :full_name
-  has_many :subject, through: :notifications
 
   validates :full_name, presence: true, uniqueness: true
   validates :github_id, uniqueness: true
+
+  def subjects
+    Subject.repository(full_name)
+  end
 end
