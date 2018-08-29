6e4dbbeddbc989387f15093a1899d08edf2578bc
Andrew Nesbitt <andrewnez@gmail.com>
2018-08-29 13:23:19 +0100

    update_labels should handle recieving json

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -18,22 +18,22 @@ class Subject < ApplicationRecord
   def update_labels(remote_labels)
     existing_labels = labels.to_a
     remote_labels.each do |l|
-      label = labels.find_by_github_id(l.id)
+      label = labels.find_by_github_id(l['id'])
       if label.nil?
         labels.create({
-          github_id: l.id,
-          color: l.color,
-          name: l.name,
+          github_id: l['id'],
+          color: l['color'],
+          name: l['name'],
         })
       else
-        label.github_id = l.id # smoothly migrate legacy labels
-        label.color = l.color
-        label.name = l.name
+        label.github_id = l['id'] # smoothly migrate legacy labels
+        label.color = l['color']
+        label.name = l['name']
         label.save if label.changed?
       end
     end
 
-    remote_label_ids = remote_labels.map(&:id)
+    remote_label_ids = remote_labels.map{|l| l['id'] }
     deleted_labels = existing_labels.reject{|l| remote_label_ids.include?(l.github_id) }
     deleted_labels.each(&:destroy)
   end
