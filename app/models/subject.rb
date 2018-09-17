44d7aa52f854ab45cf2c92f3311c5edf01d858e4
Andrew Nesbitt <andrewnez@gmail.com>
2018-09-17 15:51:49 +0100

    Delay syncing involved users after webhook event by 1 minute (#956)
    For very busy users we end syncing their account much more often that 
    required, this adds a slight buffer to reduce excess syncing.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -43,7 +43,7 @@ class Subject < ApplicationRecord
 
   def sync_involved_users
     return unless Octobox.github_app?
-    involved_user_ids.each { |user_id| SyncNotificationsWorker.perform_async_if_configured(user_id) }
+    involved_user_ids.each { |user_id| SyncNotificationsWorker.perform_in(1.minute, user_id) }
   end
 
   def self.sync(remote_subject)
