dfd7b946394704024b1869b96294d80b7e8355e8
Andrew Nesbitt <andrewnez@gmail.com>
2018-09-19 11:56:28 +0100

    Only speculatively sync users who've not been synced within the last minute (#966)

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -78,7 +78,9 @@ class Subject < ApplicationRecord
   end
 
   def involved_user_ids
-    (user_ids + Array(repository.try(:user_ids))).uniq
+    ids = users.pluck(:id)
+    ids += repository.users.not_recently_synced.pluck(:id) if repository.present?
+    ids.uniq
   end
 
   def author_url_path
