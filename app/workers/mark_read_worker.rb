6fd2f9c7aec3e7c43ef6dec97bc00a6132b6cfb4
Andrew Nesbitt <andrewnez@gmail.com>
2018-09-19 14:39:06 +0100

    Mark notifications as read on github in the background if sidekiq configured (#969)

---
+++ app/workers/mark_read_worker.rb
@@ -0,0 +1,9 @@
+class MarkReadWorker
+  include Sidekiq::Worker
+  sidekiq_options queue: :sync_notifications, unique: :until_and_while_executing
+
+  def perform(user_id, notification_ids)
+    user = User.find_by_id(user_id)
+    Notification.mark_read_on_github(user, notification_ids) if user
+  end
+end
