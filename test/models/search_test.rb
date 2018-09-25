6a1ae7b27f35379f96268fb0f2c4ca95d3153f88
Andrew Nesbitt <andrewnez@gmail.com>
2018-09-25 11:30:52 +0100

    Add user and org as aliases for owner search prefix
    Re https://github.com/octobox/octobox/issues/778#issuecomment-424093900

--- test/models/search_test.rb
+++ test/models/search_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class SearchTest < ActiveSupport::TestCase
-  test 'parses search query' do
-    # TODO
+  test 'user acts as an alias for owner' do
+    search = Search.new(query: 'user:andrew', scope: Notification.all)
+    assert_equal search.send(:owner), ['andrew']
+  end
+
+  test 'org acts as an alias for owner' do
+    search = Search.new(query: 'org:octobox', scope: Notification.all)
+    assert_equal search.send(:owner), ['octobox']
+  end
+
+  test '-user acts as an alias for -owner' do
+    search = Search.new(query: '-user:andrew', scope: Notification.all)
+    assert_equal search.send(:exclude_owner), ['andrew']
+  end
+
+  test '-org acts as an alias for -owner' do
+    search = Search.new(query: '-org:octobox', scope: Notification.all)
+    assert_equal search.send(:exclude_owner), ['octobox']
   end
 end
