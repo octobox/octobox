046e3f519d4bdbb349e10cb446141d2ea07a2d52
Andrew Nesbitt <andrewnez@gmail.com>
2018-10-05 12:56:03 +0100

    Add a rake task to sync app installation data from GitHub (#1061)

--- app/workers/sync_installation_worker.rb
+++ app/workers/sync_installation_worker.rb
@@ -5,18 +5,7 @@ class SyncInstallationWorker
   sidekiq_options queue: :sync_subjects, unique: :until_and_while_executing
 
   def perform(payload)
-    app_installation = AppInstallation.create({
-      github_id: payload['installation']['id'],
-      app_id: payload['installation']['app_id'],
-      account_login: payload['installation']['account']['login'],
-      account_id: payload['installation']['account']['id'],
-      account_type: payload['installation']['account']['type'],
-      target_type: payload['installation']['target_type'],
-      target_id: payload['installation']['target_id'],
-      permission_pull_requests: payload['installation']['permissions']['pull_requests'],
-      permission_issues: payload['installation']['permissions']['issues'],
-      permission_statuses: payload['installation']['permissions']['statuses']
-    })
+    app_installation = AppInstallation.create(AppInstallation.map_from_api(payload['installation']))
 
     app_installation.add_repositories(payload['repositories'])
   end
