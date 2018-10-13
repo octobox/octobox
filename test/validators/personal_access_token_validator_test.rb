951068bb62ec94780f34b0edf2db4b0006646190
Andy Stabler <andy_60@hotmail.co.uk>
2018-10-13 21:38:29 +0100

    Refactor user validations (#1090)
    * Refactor user validations
    
    Extracts methods for each validation around personal access tokens
    
    Addresses the following Rubocop issue:
    
    app/models/user.rb:123:3: C: Metrics/CyclomaticComplexity: Cyclomatic complexity for personal_access_token_validator is too high. [8/6]
      def personal_access_token_validator ...
      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    * Extract PersonalAccessTokenValidator out of User
    
    The token validation logic was quite noisy in the User class, so this
    commit pulls it out into its own class.
    
    As part of this we have updated the validation to not return early if
    validation fails—now all validation errors will be added to the errors object.

---
+++ test/validators/personal_access_token_validator_test.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+require 'test_helper'
+
+class PersonalAccessTokenValidatorTest < ActiveSupport::TestCase
+  setup do
+    @user = build(:user, personal_access_token: '1234')
+    stub_user_request(user: @user)
+    stub_personal_access_tokens_enabled
+    stub_notifications_request
+  end
+
+  def assert_error_present(model_object, error)
+    refute model_object.valid?
+    assert model_object.errors[error[0]].include? error[1]
+  end
+
+  test 'it allows a blank personal_access_token' do
+    @user.personal_access_token = nil
+    assert @user.valid?
+  end
+
+  test 'does not allow setting personal_access_token without being enabled' do
+    stub_personal_access_tokens_enabled(value: false)
+    assert_error_present(@user, PersonalAccessTokenValidator::ERRORS[:disallowed_tokens])
+  end
+
+  test 'does not allow invalid credentials' do
+    Octokit::Client.any_instance.stubs(:validate_credentials).returns(false)
+    stub_user_request(user: @user)
+    assert_error_present(@user, PersonalAccessTokenValidator::ERRORS[:invalid_token])
+  end
+
+  test 'does not allow a personal_access_token for another user' do
+    stub_user_request(body: '{"id": 98}')
+    assert_error_present(@user, PersonalAccessTokenValidator::ERRORS[:invalid_token])
+  end
+
+  test 'does not allow a personal_access_token without the notifications scope' do
+    stub_user_request(user: @user, oauth_scopes: 'user, repo')
+    assert_error_present(@user, PersonalAccessTokenValidator::ERRORS[:missing_notifications_scope])
+  end
+
+  test 'does not allow a personal_access_token without the read:org scope if restricted_access enabled' do
+    stub_restricted_access_enabled
+    stub_user_request(oauth_scopes: 'user, repo')
+    assert_error_present(@user, PersonalAccessTokenValidator::ERRORS[:missing_read_org_scope])
+  end
+end
