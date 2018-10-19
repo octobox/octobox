c155d355dce663779dbcfb75734acf1c579efa14
Andrew Nesbitt <andrewnez@gmail.com>
2018-10-19 15:55:40 +0100

    Gracefully handle subject syncing jobs where the notification has been deleted (#1129)
    Usually occurs when someone deletes their account whilst there are still 
    jobs in the queue.

--- app/workers/update_subject_worker.rb
+++ app/workers/update_subject_worker.rb
@@ -5,6 +5,6 @@ class UpdateSubjectWorker
   sidekiq_options queue: :sync_subjects, unique: :until_and_while_executing
 
   def perform(notification_id, force = false)
-    Notification.find(notification_id).try(:update_subject_in_foreground, force)
+    Notification.find_by_id(notification_id).try(:update_subject_in_foreground, force)
   end
 end
