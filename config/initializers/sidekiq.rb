octobox/octobox

commit 35c31bac1a0ac838dea5d5cb2cdfe40779c25f43

Cleanup unique jobs digest after death (#1234)
Should stop users being unable to resync after a previous sync job has 
died

config/initializers/sidekiq.rb
@@ -16,6 +16,10 @@ if Octobox.background_jobs_enabled?
     Sidekiq::Status.configure_client_middleware config, expiration: 60.minutes
   end
 
+  config.death_handlers << ->(job, _ex) do
+    SidekiqUniqueJobs::Digests.del(digest: job['unique_digest']) if job['unique_digest']
+  end
+
   Sidekiq.configure_client do |config|
     config.redis = { url: Octobox.config.redis_url }
     Sidekiq::Status.configure_client_middleware config, expiration: 60.minutes

commit c4e89ef82178535c52495d81e2bc301741726ee2

Fix sidekiq initializer
Fixes what I broke in https://github.com/octobox/octobox/pull/1234

config/initializers/sidekiq.rb
@@ -14,10 +14,10 @@ if Octobox.background_jobs_enabled?
     end
     Sidekiq::Status.configure_server_middleware config, expiration: 60.minutes
     Sidekiq::Status.configure_client_middleware config, expiration: 60.minutes
-  end
 
-  config.death_handlers << ->(job, _ex) do
-    SidekiqUniqueJobs::Digests.del(digest: job['unique_digest']) if job['unique_digest']
+    config.death_handlers << ->(job, _ex) do
+      SidekiqUniqueJobs::Digests.del(digest: job['unique_digest']) if job['unique_digest']
+    end
   end
 
   Sidekiq.configure_client do |config|