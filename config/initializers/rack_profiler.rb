224586b4ed46bb6d4345e89b693a4668f59695c5
Mark Tareshawty <tarebyte@github.com>
2018-12-07 15:57:41 -0500

    Configure Rack::MiniProfiler

---
+++ config/initializers/rack_profiler.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+unless Rails.env.test?
+  require "rack-mini-profiler"
+
+  # Initialization is skipped so trigger it
+  Rack::MiniProfilerRails.initialize!(Rails.application)
+
+  Rack::MiniProfiler.config.position = "bottom-right"
+
+  # Set Redis store for production
+  if Rails.env.production?
+    uri = URI.parse(Octobox.config.redis_url)
+
+    Rack::MiniProfiler.config.storage_options = { host: uri.host, port: uri.port, password: uri.password }
+    Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+  else
+    # Set MemoryStore for development
+    Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+  end
+end
