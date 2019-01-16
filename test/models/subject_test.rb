octobox/octobox

commit 459fd3faf13bc62a460ec1d77a0ba7ccd3779042

Strip null byte characters from subject and comment bodies before saving (#1480)

app/models/subject.rb
@@ -69,7 +69,7 @@ class Subject < ApplicationRecord
       assignees: ":#{Array(remote_subject['assignees'].try(:map) {|a| a['login'] }).join(':')}:",
       locked: remote_subject['locked'],
       sha: remote_subject.fetch('head', {})['sha'],
-      body: remote_subject['body']
+      body: remote_subject['body'].try(:delete!, "\u0000")
     })
 
     return unless subject.persisted?
@@ -116,7 +116,7 @@ class Subject < ApplicationRecord
     remote_comments.each do |remote_comment|
       comments.find_or_create_by(github_id: remote_comment.id) do |comment|
         comment.author = remote_comment.user.login
-        comment.body = remote_comment.body
+        comment.body = remote_comment.body.try(:delete!, "\u0000")
         comment.author_association = remote_comment.author_association
         comment.created_at = remote_comment.created_at
         comment.save

commit a8ab4c660cce61b24433f067ffc7f91ebcc44c00

Fix syncing of subject and comment body
Bug introduced in #1480

app/models/subject.rb
@@ -69,7 +69,7 @@ class Subject < ApplicationRecord
       assignees: ":#{Array(remote_subject['assignees'].try(:map) {|a| a['login'] }).join(':')}:",
       locked: remote_subject['locked'],
       sha: remote_subject.fetch('head', {})['sha'],
-      body: remote_subject['body'].try(:delete!, "\u0000")
+      body: remote_subject['body'].try(:gsub, "\u0000", '')
     })
 
     return unless subject.persisted?
@@ -116,7 +116,7 @@ class Subject < ApplicationRecord
     remote_comments.each do |remote_comment|
       comments.find_or_create_by(github_id: remote_comment.id) do |comment|
         comment.author = remote_comment.user.login
-        comment.body = remote_comment.body.try(:delete!, "\u0000")
+        comment.body = remote_comment.body.try(:gsub, "\u0000", '')
         comment.author_association = remote_comment.author_association
         comment.created_at = remote_comment.created_at
         comment.save

test/models/subject_test.rb
@@ -40,6 +40,13 @@ class SubjectTest < ActiveSupport::TestCase
     assert_equal 2, subject.comment_count
   end
 
+  test 'sync updates body of an issue' do
+    remote_subject = Oj.load(File.open(file_fixture('subject_56.json')))
+    Subject.sync(remote_subject)
+    subject = Subject.first
+    assert subject.body.present?
+  end
+
   test "sync doesnt update comment_count of releases" do
     remote_subject = Oj.load(File.open(file_fixture('release.json')))
     Subject.sync(remote_subject)