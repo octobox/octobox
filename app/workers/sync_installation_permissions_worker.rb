e03f3fec98d37a62436c464abfec6e52bc4a6e56
Andrew Nesbitt <andrewnez@gmail.com>
2019-01-16 14:41:48 +0000

    Change queues of some sidekiq jobs
    Avoids some quick/important jobs getting stuck in the sync_subjects or 
    sync_notification queues.
    
    cc https://github.com/octobox/octobox/issues/1277

--- app/workers/sync_installation_permissions_worker.rb
+++ app/workers/sync_installation_permissions_worker.rb
@@ -2,7 +2,7 @@
 
 class SyncInstallationPermissionsWorker
   include Sidekiq::Worker
-  sidekiq_options queue: :sync_subjects, unique: :until_and_while_executing
+  sidekiq_options queue: :marketplace, unique: :until_and_while_executing
 
   def perform(user_id)
     User.find_by_id(user_id).try(:sync_app_installation_access)
