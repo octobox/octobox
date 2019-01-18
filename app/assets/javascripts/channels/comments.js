93cf5f41f6f8a75a335d6d8bc01540ab286d17f2
benjam <ben@octobox.io>
2019-01-18 15:31:47 +0000

    Unsubscribe if we already have a subscription registered

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -1,5 +1,8 @@
 if ($("meta[name='push_notifications']").length >0) {
   $(document).on('click', '.thread-link', function(){
+    if(App.comments){
+      App.comments.unsubscribe();
+    }
     App.comments = App.cable.subscriptions.create({
       channel: "CommentsChannel",
       notification: $(this).attr('href').split('/').pop()},{
