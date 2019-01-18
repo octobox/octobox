88b4e7d572bfb58c9566f11178a4870f0b076f1e
benjam <ben@octobox.io>
2019-01-18 11:42:46 +0000

    fix subscription class

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -1,9 +1,11 @@
 if ($("meta[name='push_notifications']").length >0) {
   $(document).on('click', '.thread-link', function(){
 
-    App.comments = App.cable.subscriptions.create({channel: "CommentsChannel", 
-      notification: $(this).attr('href').split('/').pop(),
+    App.comments = App.cable.subscriptions.create({
+      channel: "CommentsChannel", 
+      notification: $(this).attr('href').split('/').pop()},{
       received: function(data){
+        console.log(data)
         if($(data.id).length) {
           if ($('notification-thread').attr('data-id') == data.subject){
             $('.discussion-thread').append(data.comment_html);
