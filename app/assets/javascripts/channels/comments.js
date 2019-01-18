f19afaad86a499bf32a3954ed38f0dc05735dc8a
benjam <ben@octobox.io>
2019-01-18 15:42:01 +0000

    Add a turbolinks handler for refreshes on a notification page

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -14,3 +14,22 @@ if ($("meta[name='push_notifications']").length >0) {
     });
   });
 }
+
+if ($("meta[name='push_notifications']").length >0) {
+  $(document).on("turbolinks:load", function(){
+    if($('#thread').is(':visible')){
+      if(App.comments){
+        App.comments.unsubscribe();
+      }
+      App.comments = App.cable.subscriptions.create({
+        channel: "CommentsChannel",
+        notification: $(location).attr('href').split('/').pop()},{
+        received: function(data){
+          if ($('#notification-thread').attr('data-id') == data.subject_id){
+            $('.discussion-thread').append(data.comment_html);
+          }
+        }
+      });
+    }
+  });
+}
