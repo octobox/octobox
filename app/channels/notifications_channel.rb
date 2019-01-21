e7b542f657c0530061a63c542c68f1763aee0cac
benjam <ben@octobox.io>
2019-01-21 14:00:38 +0000

    fix basic test

--- app/channels/notifications_channel.rb
+++ app/channels/notifications_channel.rb
@@ -1,7 +1,10 @@
 class NotificationsChannel < ApplicationCable::Channel
   def subscribed
-  	reject if current_user.id.nil?
-  	stream_from "notifications:#{current_user.id}"
+  	if current_user.nil? 
+  			reject
+  	else
+  		stream_from "notifications:#{current_user.id}"
+  	end
   end
 
   def unsubscribed
