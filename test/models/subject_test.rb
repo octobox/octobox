620821bab08d8e8c02b1c8a42056dd8297bcc27d
Andy Stabler <andy_60@hotmail.co.uk>
2019-01-31 22:03:44 +0000

    Add coverage to Subject (#1428)

--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -12,7 +12,7 @@ class SubjectTest < ActiveSupport::TestCase
     response = { status: 200, body: file_fixture('status_success.json'), headers: { 'Content-Type' => 'application/json' } }
     stub_request(:get, url).and_return(response)
 
-  	Subject.sync_status(sha, 'octobox/octobox')
+    Subject.sync_status(sha, 'octobox/octobox')
 
     subject.reload
 
@@ -47,10 +47,40 @@ class SubjectTest < ActiveSupport::TestCase
     assert subject.body.present?
   end
 
-  test "sync doesnt update comment_count of releases" do
+  test "sync doesn't update comment_count of releases" do
     remote_subject = Oj.load(File.open(file_fixture('release.json')))
     Subject.sync(remote_subject)
     subject = Subject.first
     assert_nil subject.comment_count
+  end
+
+  test 'sync updates a matching subject' do
+    remote_subject = Oj.load(File.open(file_fixture('subject_56.json')))
+    subject = create(:subject, url: remote_subject['url'], comment_count: nil)
+    Subject.expects(:find_or_create_by).returns(subject)
+    Subject.sync(remote_subject)
+    assert_equal 8, subject.comment_count
+  end
+
+  test "sync creates a new subject when a matching one can't be found" do
+    assert_equal 0, Subject.count
+    remote_subject = Oj.load(File.open(file_fixture('subject_56.json')))
+    Subject.sync(remote_subject)
+    assert_equal 1, Subject.count
+  end
+
+  test 'sync sets the full name when there is repository info' do
+    remote_subject = Oj.load(File.open(file_fixture('subject_56.json')))
+    remote_subject['repository'] = {'full_name' => 'repository full name'}
+    Subject.sync(remote_subject)
+    assert_equal 'repository full name', Subject.last.repository_full_name
+  end
+
+  test 'sync sets the full name when there is no repository info' do
+    remote_subject = Oj.load(File.open(file_fixture('subject_56.json')))
+    assert_nil remote_subject['repository']
+    remote_subject['repository'] = {'full_name' => 'repository full name'}
+    Subject.sync(remote_subject)
+    assert_equal 'repository full name', Subject.last.repository_full_name
   end
 end
