412c8fbb6ea26e4f664a013a168e15a277025e5e
Jason Hsu <jhsu802701@jasonhsu.com>
2019-02-19 02:51:31 -0600

    db/seeds.rb now seeds database with users (#1599)

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#######################
+# BEGIN: creating users
+#######################
+
+n_users = 100
+n_users.times do |n|
+  User.create!(github_id: n + 1,
+               github_login: "github_login_#{n + 1}")
+end
+
+##########################
+# FINISHED: creating users
+##########################
