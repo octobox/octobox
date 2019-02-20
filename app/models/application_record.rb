8e846faff1e9e7db697f7609765ed90cc9dc3e39
Andrew Nesbitt <andrewnez@gmail.com>
2019-02-20 10:56:55 +0000

    fast_total should fallback to count on mysql

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,10 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def self.fast_total
-    ActiveRecord::Base.count_by_sql "SELECT (reltuples)::integer FROM pg_class r WHERE relkind = 'r' AND relname = '#{self.table_name}'"
+    if DatabaseConfig.is_postgres?
+      ActiveRecord::Base.count_by_sql "SELECT (reltuples)::integer FROM pg_class r WHERE relkind = 'r' AND relname = '#{self.table_name}'"
+    else
+      self.count
+    end
   end
 end
