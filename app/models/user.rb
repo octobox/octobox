ce5efc4a2b557a4208c0bfb59e055c80afde5727
benjam <ben@octobox.io>
2019-02-25 15:36:32 +0000

    typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -166,7 +166,7 @@ class User < ApplicationRecord
 
   def create_default_pinned_searches 
     pinned_searches.new(query: 'state:closed,merged archived:false', name: 'Archivable')
-    pinned_searches.new(query: 'type:pull_request state:open status:success', name: 'Mergable')
+    pinned_searches.new(query: 'type:pull_request state:open status:success', name: 'Mergeable')
     pinned_searches.new(query: "type:pull_request author:#{github_login} inbox:true", name: 'My PRs')
     save
   end
