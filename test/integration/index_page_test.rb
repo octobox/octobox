10e8913a512aaf629c844eb1e1ef6fb043f0a1c9
Jason Hsu <jhsu802701@jasonhsu.com>
2019-02-26 01:33:02 -0600

    Added tests of the home page given OCTOBOX_IO and GITHUB_APP_ID values; added tests of the pricing page (#1639)

--- test/integration/index_page_test.rb
+++ test/integration/index_page_test.rb
@@ -1,6 +1,21 @@
 require 'test_helper'
 
 class IndexPageTest < ActionDispatch::IntegrationTest
+  def has_open_collective
+    assert_select 'h5', text: 'Support the community'
+    assert_select 'h6', text: 'Pay by donation on Open Collective.'
+    assert_match 'Unlimited private repositories', response.body
+    assert_match 'Unlimited public repositories', response.body
+    assert_match 'Unlimited collaborators', response.body
+    assert_match 'Next: make your donation on Open Collective', response.body
+  end
+
+  def has_github
+    assert_select 'h5', text: 'Support the company'
+    assert_select 'h6', text: 'Buy from the GitHub marketplace.'
+    assert_match 'Next: confirm your purchase on GitHub', response.body
+  end
+
   test 'index page without login is home page' do
     get root_path
     assert_select "a[href=?]", login_path, text: 'Sign in'
@@ -34,5 +49,59 @@ class IndexPageTest < ActionDispatch::IntegrationTest
     assert_match 'Read', response.body
     assert_match 'Unread', response.body
     assert_match 'Last sync', response.body
+  end
+
+  test 'home page shows GitHub section if GITHUB_APP_ID is set' do
+    set_env('GITHUB_APP_ID', '12345') do
+      get root_path
+      assert_select 'h3', text: 'Liven up notifications with the GitHub app'
+      assert_match 'Octobox will pull basic public and private notification', response.body
+      assert_match 'Install the GitHub app', response.body
+    end
+  end
+
+  test 'home page shows Octobox section if OCTOBOX_IO is set to true' do
+    set_env('OCTOBOX_IO', 'true') do
+      get root_path
+      assert_match 'notifications managed, and counting', response.body
+      assert_select 'h3', text: 'Pricing'
+      assert_match 'free for open source', response.body
+      assert_match 'basic notifications for private projects', response.body
+
+      has_open_collective
+      has_github
+
+      assert_select 'h3', text: 'Used by developers from'
+    end
+  end
+
+  test 'home page has link to the pricing page' do
+    set_env('OCTOBOX_IO', 'true') do
+      get root_path
+      assert_select "a[href=?]", '/pricing#why', text: 'Read more about our pricing strategy'
+    end
+  end
+
+  test 'The pricing page has the expected content' do
+    set_env('OCTOBOX_IO', 'true') do
+      get pricing_path
+      assert_select 'h1', text: 'Octobox'
+      assert_select 'h3', text: 'One product, two ways to pay'
+      assert_match 'free for open source', response.body
+      assert_match 'access to private projects', response.body
+
+      has_open_collective
+      has_github
+
+      assert_select 'h2', text: 'What... Why?'
+      assert_select 'h5', text: 'An experiment in open source sustainability'
+      assert_match 'Octobox is primarily maintained by Andrew Nesbitt and Benjamin Nickolls.', response.body
+
+      assert_select 'h5', text: 'Do you prefer supporting a commercial provider, or the community itself?'
+      assert_match 'Octobox.io operated by Octobox Ltd.', response.body
+
+      assert_select 'h5', text: 'Why does this matter?'
+      assert_match 'provide a blueprint for others to follow', response.body
+    end
   end
 end
