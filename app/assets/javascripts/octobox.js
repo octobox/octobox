d32a234216c15200442b61591926f5e4365c7c7b
spalger <email@spalger.com>
2019-02-26 17:16:04 -0800

    Add delay to display of [data-toggle=popover]

--- app/assets/javascripts/octobox.js
+++ app/assets/javascripts/octobox.js
@@ -92,20 +92,34 @@ var Octobox = (function() {
   };
 
   var enablePopOvers = function() {
+    var showTimer;
+
     $('[data-toggle="popover"]').popover({ trigger: "manual" , html: true})
     .on("mouseenter", function () {
+      if (showTimer) {
+        clearTimeout(showTimer);
+      }
+
+      showTimer = setTimeout(function () {
+        showTimer = undefined
         var _this = this;
         $(this).popover("show");
         $(".popover").on("mouseleave", function () {
             $(_this).popover('hide');
         });
+      }, 2000);
     }).on("mouseleave", function () {
-        var _this = this;
-        setTimeout(function () {
-            if (!$(".popover:hover").length) {
-                $(_this).popover("hide");
-            }
-        }, 300);
+      if (showTimer) {
+        clearTimeout(showTimer);
+        return;
+      }
+
+      var _this = this;
+      setTimeout(function () {
+        if (!$(".popover:hover").length) {
+          $(_this).popover("hide");
+        }
+      }, 300);
     });
   }
 
