2a920c6bdf99260e8d42395cf69cced1eaff3cf1
Jason Hsu <jhsu802701@jasonhsu.com>
2019-03-04 03:33:19 +0000

    Added controller and integration tests for the terms page

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -27,4 +27,18 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
       assert_template 'pages/privacy'
     end
   end
+
+  test 'terms page renders the home page if OCTOBOX_IO is not set' do
+    get terms_path
+    assert_response :redirect
+    assert_redirected_to '/422'
+  end
+
+  test 'will render the terms page if OCTOBOX_IO is set to true' do
+    set_env('OCTOBOX_IO', 'true') do
+      get terms_path
+      assert_response :success
+      assert_template 'pages/terms'
+    end
+  end
 end
