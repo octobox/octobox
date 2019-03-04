166188f50aef82a36f3ddaa5ba69bceb4c496b4f
benjam <ben@octobox.io>
2019-03-04 10:03:06 +0000

    tabs

--- db/migrate/20190304093903_display_comments_in_ocotobox.rb
+++ db/migrate/20190304093903_display_comments_in_ocotobox.rb
@@ -1,5 +1,5 @@
 class DisplayCommentsInOcotobox < ActiveRecord::Migration[5.2]
-  def change
-  	change_column :users, :display_comments, :boolean, default: true
+	def change
+		change_column :users, :display_comments, :boolean, default: true
 	end
 end
