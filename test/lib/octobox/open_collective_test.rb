0c19c685e38107af0061da416ba953301f214922
Benjamin Nickolls <BenJam@users.noreply.github.com>
2019-04-25 18:53:51 +0100

    Account for plan switches in OC task (#1755)
    * Account for plan switches in OC task
    
    * checkpoint
    
    * Uncomment subscription plan removal
    * Add OC members fixture
    * Add oc rake task test outline
    * Add stub helper for oc members request
    
    * Update test plan
    
    * factory
    
    * Fixed test
    
    * Fixed another test

---
+++ test/lib/octobox/open_collective_test.rb
@@ -0,0 +1,55 @@
+require 'test_helper'
+
+class OpenCollectiveTest < ActiveSupport::TestCase
+
+	setup do
+		stub_oc_members_request
+		@user = create(:user, github_login: 'andrew', github_id: '12345')
+		@app = create(:app_installation, account_login: @user.github_login, account_id: @user.github_id)
+
+		@individual_plan = create(:subscription_plan, name: "Open Collective Individual")
+    @org_plan = create(:subscription_plan, name: "Open Collective Organisation")
+	end
+
+	test "plans are set up" do
+		assert_equal SubscriptionPlan.count, 2
+	end
+
+	test "gets subscriber names" do
+		transactions = Oj.load(Typhoeus.get("https://opencollective.com/octobox/members.json").body)
+
+		names = Octobox::OpenCollective.get_sub_names(transactions, 10)
+
+		assert_equal names.length, 4
+		assert names.include?('tom')
+	end
+
+	test "adds new plans" do
+		Octobox::OpenCollective.apply_plan([@user.github_login], @individual_plan.name)
+		assert @user.has_personal_plan?
+	end
+
+	test "renews plans" do
+		create(:subscription_purchase, account_id: @user.github_id, subscription_plan_id: @individual_plan.id)
+		Octobox::OpenCollective.apply_plan([@user.github_login], @individual_plan.name)
+		assert @user.has_personal_plan?
+	end
+
+	test "removes plans" do
+		create(:subscription_purchase, account_id: @user.github_id, subscription_plan_id: @individual_plan.id, unit_count: 1)
+		Octobox::OpenCollective.sync
+		refute @user.has_personal_plan?
+	end
+
+	test "upgrades plans" do
+
+		@repo = create(:repository, app_installation_id: @app.id, private: true)
+		@notification = create(:notification, repository: @repo, user: @user)
+
+		create(:subscription_purchase, account_id: @user.github_id, subscription_plan_id: @individual_plan.id, unit_count: 1)
+		Octobox::OpenCollective.sync
+
+		refute @user.has_personal_plan?
+		refute @notification.upgrade_required?
+	end
+end
