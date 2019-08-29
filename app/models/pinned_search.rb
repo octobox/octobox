674a91ae0d046193c1ed44e40101e78a73477b8d
Julian Nadeau <jules2689@github.com>
2019-08-29 16:07:31 -0400

    Format pinned search on save to ensure consistent formatting

--- app/models/pinned_search.rb
+++ app/models/pinned_search.rb
@@ -5,4 +5,12 @@ class PinnedSearch < ApplicationRecord
   validates :user_id, presence: true
   validates :query, presence: true
   validates :name, presence: true
+
+  before_commit :format_query, on: [:create, :update]
+
+  def format_query
+    return unless self.query.present?
+    # ensures consistent formatting by formatting with Search.new
+    self.query = Search.new(query: self.query, scope: {}).to_query
+  end
 end
