fac77053d7d0453d98e93148ea1138d6810163e2
Andy Stabler <andy_60@hotmail.co.uk>
2019-10-14 21:06:25 +0100

    Extract sync instance method
    Lots of the stuff going on inside the Subject.sync method is acting on
    an instance and not doing things at the class level. This commit extracts
    a Subject#sync instance method to make things more object oriented and
    to allow us to start extracting the logic out of this method. This
    should hopefully make things easier to understand what's going on.

--- test/models/subject_test.rb
+++ test/models/subject_test.rb
@@ -26,8 +26,8 @@ class SubjectTest < ActiveSupport::TestCase
 
   test 'sync updates comment_count of an issue' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    subject = Subject.first
+    subject = create(:subject)
+    subject.sync(remote_subject)
     assert_equal 8, subject.comment_count
   end
 
@@ -38,39 +38,36 @@ class SubjectTest < ActiveSupport::TestCase
 
     user = create(:user)
     create(:notification, subject_url:remote_subject['url'], user: user)
-    create(:subject, url: remote_subject['url'])
-
-    Subject.sync(remote_subject)
-    subject = Subject.first
+    subject = create(:subject, url: remote_subject['url'])
+    subject.sync(remote_subject)
     assert_equal 0, subject.comment_count
   end
 
   test 'sync updates comment_count of a commit' do
     remote_subject = load_subject('commit_no_author.json')
-    Subject.sync(remote_subject)
-    subject = Subject.first
+    subject = create(:subject)
+    subject.sync(remote_subject)
     assert_equal 2, subject.comment_count
   end
 
   test 'sync updates body of an issue' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    subject = Subject.first
+    subject = create(:subject)
+    subject.sync(remote_subject)
     assert subject.body.present?
   end
 
   test "sync doesn't update comment_count of releases" do
     remote_subject = load_subject('release.json')
-    Subject.sync(remote_subject)
-    subject = Subject.first
+    subject = create(:subject, comment_count: nil)
+    subject.sync(remote_subject)
     assert_nil subject.comment_count
   end
 
   test 'sync updates a matching subject' do
     remote_subject = load_subject('subject_56.json')
     subject = create(:subject, url: remote_subject['url'], comment_count: nil)
-    Subject.expects(:find_or_create_by).returns(subject)
-    Subject.sync(remote_subject)
+    subject.sync(remote_subject)
     assert_equal 8, subject.comment_count
   end
 
@@ -84,7 +81,8 @@ class SubjectTest < ActiveSupport::TestCase
   test 'sync sets the full name when there is repository info' do
     remote_subject = load_subject('subject_56.json')
     remote_subject['repository'] = {'full_name' => 'repository full name'}
-    Subject.sync(remote_subject)
+    subject = create(:subject)
+    subject.sync(remote_subject)
     assert_equal 'repository full name', Subject.last.repository_full_name
   end
 
@@ -92,61 +90,68 @@ class SubjectTest < ActiveSupport::TestCase
     remote_subject = load_subject('subject_56.json')
     assert_nil remote_subject['repository']
     remote_subject['repository'] = {'full_name' => 'repository full name'}
-    Subject.sync(remote_subject)
-    assert_equal 'repository full name', Subject.last.repository_full_name
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 'repository full name', subject.repository_full_name
   end
 
   test 'sync sets the github_id' do
     remote_subject = load_subject('subject_56.json')
     expected_github_id = 5
     remote_subject['id'] = expected_github_id
-    Subject.sync(remote_subject)
-    assert_equal expected_github_id, Subject.last.github_id
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal expected_github_id, subject.github_id
   end
 
   test 'sync sets the state when the subject is merged' do
     stub_comments_requests
-    
+
     remote_subject = load_subject('merged_pull_request.json')
-    
+
     user = create(:user)
     create(:notification, subject_url:remote_subject['url'], user: user)
-    create(:subject, url: remote_subject['url'])
+    subject = create(:subject, url: remote_subject['url'])
 
     refute_empty remote_subject['merged_at']
-    Subject.sync(remote_subject)
-    assert_equal 'merged', Subject.last.state
+    subject.sync(remote_subject)
+    assert_equal 'merged', subject.state
   end
 
   test 'sync sets the state' do
     remote_subject = load_subject('subject_56.json')
     assert_nil remote_subject['merged_at']
-    Subject.sync(remote_subject)
-    assert_equal 'closed', Subject.last.state
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 'closed', subject.state
   end
 
   test 'sync sets the author' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_equal 'andrew', Subject.last.author
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 'andrew', subject.author
   end
 
   test "sync sets the author to nil when it's not present" do
     remote_subject = load_subject('commit_no_author.json')
-    Subject.sync(remote_subject)
-    assert_nil Subject.last.author
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_nil subject.author
   end
 
   test 'sync sets the html_url' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_equal 'https://github.com/octobox/octobox/issues/56', Subject.last.html_url
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 'https://github.com/octobox/octobox/issues/56', subject.html_url
   end
 
   test 'sync sets the created_at value' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_equal DateTime.parse('2016-12-19T21:30:41Z'), Subject.last.created_at
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal DateTime.parse('2016-12-19T21:30:41Z'), subject.created_at
   end
 
   test "sync sets a default created_at value when it's not present" do
@@ -154,14 +159,16 @@ class SubjectTest < ActiveSupport::TestCase
     travel_to created_at
     remote_subject = load_subject('commit_no_author.json')
     assert_nil remote_subject['created_at']
-    Subject.sync(remote_subject)
-    assert_equal created_at, Subject.last.created_at
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal created_at, subject.created_at
   end
 
   test 'sync sets the updated_at value' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_equal DateTime.parse('2016-12-27T20:10:18Z'), Subject.last.updated_at
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal DateTime.parse('2016-12-27T20:10:18Z'), subject.updated_at
   end
 
   test "sync sets a default updated_at value when it's not present" do
@@ -169,40 +176,46 @@ class SubjectTest < ActiveSupport::TestCase
     travel_to updated_at
     remote_subject = load_subject('commit_no_author.json')
     assert_nil remote_subject['updated_at']
-    Subject.sync(remote_subject)
-    assert_equal updated_at, Subject.last.updated_at
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal updated_at, subject.updated_at
   end
 
   test 'sync sets the comment_count' do
     remote_subject = load_subject('subject_56.json')
     refute_nil(remote_subject['comments'])
-    Subject.sync(remote_subject)
-    assert_equal 8, Subject.last.comment_count
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 8, subject.comment_count
   end
 
   test 'sync sets the comment_count for commits' do
     remote_subject = load_subject('commit_no_author.json')
     assert_nil(remote_subject['comments'])
-    Subject.sync(remote_subject)
-    assert_equal 2, Subject.last.comment_count
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 2, subject.comment_count
   end
 
   test 'sync sets the assignees' do
     remote_subject = load_subject('subject_57.json')
-    Subject.sync(remote_subject)
-    assert_equal ':andrew:', Subject.last.assignees
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal ':andrew:', subject.assignees
   end
 
   test 'sync sets an empty assignees value when there are non present' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_equal '::', Subject.last.assignees
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal '::', subject.assignees
   end
 
   test 'sync sets the locked value' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_equal false, Subject.last.locked
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal false, subject.locked
   end
 
   test 'sync sets the sha' do
@@ -211,17 +224,19 @@ class SubjectTest < ActiveSupport::TestCase
     remote_subject = load_subject('merged_pull_request.json')
 
     user = create(:user)
-    create(:notification, subject_url:remote_subject['url'], user: user)
+    create(:notification, subject_url: remote_subject['url'], user: user)
     create(:subject, url: remote_subject['url'])
 
-    Subject.sync(remote_subject)
-    assert_equal '84b4e75e5f627d34f7a85982bda7b260f34db4dd', Subject.last.sha
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal '84b4e75e5f627d34f7a85982bda7b260f34db4dd', subject.sha
   end
 
   test "sync sets the sha to nil when it's not present" do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    assert_nil Subject.last.sha
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_nil subject.sha
   end
 
   test 'sync sets the body' do
@@ -234,31 +249,34 @@ class SubjectTest < ActiveSupport::TestCase
     create(:notification, subject_url:remote_subject['url'], user: user)
     create(:subject, url: remote_subject['url'])
 
-    Subject.sync(remote_subject)
-    assert_equal 'Fixed this thing', Subject.last.body
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    assert_equal 'Fixed this thing', subject.body
   end
 
   test 'sync updates labels when the subjects is persisted and labels are present in the remote subject' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:update_labels)
-    Subject.sync(remote_subject)
-    assert_equal 1, Subject.count
+    subject = create(:subject)
+    subject.expects(:update_labels)
+    subject.sync(remote_subject)
   end
 
   test 'sync does not update labels when the subject is not persisted' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:valid?).at_least_once.returns(false)
-    Subject.any_instance.expects(:update_labels).never
-    Subject.sync(remote_subject)
+    subject = build(:subject)
+    subject.expects(:valid?).at_least_once.returns(false)
+    subject.expects(:update_labels).never
+    subject.sync(remote_subject)
     assert_equal 0, Subject.count
   end
 
   test 'sync updates the comments when the subject is persisted and comments are available' do
     remote_subject = load_subject('subject_56.json')
+    subject = build(:subject)
     Octobox.expects(:include_comments?).returns(true)
-    Subject.any_instance.expects(:update_comments)
-    Subject.sync(remote_subject)
-    assert_equal 1, Subject.count
+    subject.expects(:update_comments)
+    subject.sync(remote_subject)
+    assert subject.persisted?
   end
 
   test 'sync updates reviews and review comemnts when the subject is a pull request' do
@@ -267,11 +285,11 @@ class SubjectTest < ActiveSupport::TestCase
 
     user = create(:user)
     create(:notification, subject_url:remote_subject['url'], user: user)
-    create(:subject, url: remote_subject['url'])
+    subject = create(:subject, url: remote_subject['url'])
 
-    Subject.sync(remote_subject)
+    subject.sync(remote_subject)
 
-    assert_equal 24, Subject.last.comments.count
+    assert_equal 24, subject.comments.count
   end
 
   test 'review states are saved is there is a review with a state' do
@@ -290,68 +308,75 @@ class SubjectTest < ActiveSupport::TestCase
 
   test 'sync does not update comments when subject is not persisted' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:valid?).at_least_once.returns(false)
-    Subject.any_instance.expects(:update_comments).never
-    Subject.sync(remote_subject)
+    subject = build(:subject)
+    subject.expects(:valid?).at_least_once.returns(false)
+    subject.expects(:update_comments).never
+    subject.sync(remote_subject)
     assert_equal 0, Subject.count
   end
 
   test 'sync does not update comments when comments are disabled' do
     remote_subject = load_subject('subject_56.json')
     Octobox.expects(:include_comments?).at_least_once.returns(false)
-    Subject.any_instance.expects(:update_comments).never
-    Subject.sync(remote_subject)
+    subject = create(:subject)
+    subject.expects(:update_comments).never
+    subject.sync(remote_subject)
   end
 
   test 'sync does not update comments when the subejct has no comments' do
     remote_subject = load_subject('release.json')
+    subject = create(:subject)
     Octobox.expects(:include_comments?).at_least_once.returns(true)
-    Subject.any_instance.expects(:update_comments).never
-    Subject.sync(remote_subject)
+    subject.expects(:update_comments).never
+    subject.sync(remote_subject)
   end
 
   test 'sync updates the status when the subject is persisted' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:update_status)
-    Subject.sync(remote_subject)
-    assert_equal 1, Subject.count
+    subject = create(:subject)
+    subject.expects(:update_status)
+    subject.sync(remote_subject)
   end
 
   test 'sync does not update the status when the subject is not persisted' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:valid?).at_least_once.returns(false)
-    Subject.any_instance.expects(:update_status).never
-    Subject.sync(remote_subject)
-    assert_equal 0, Subject.count
+    subject = build(:subject)
+    subject.expects(:valid?).at_least_once.returns(false)
+    subject.expects(:update_status).never
+    subject.sync(remote_subject)
+    refute subject.persisted?
   end
 
   test 'sync updates involved users when the subject is persisted and there are notifiable fields that have changed' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:sync_involved_users)
-    Subject.sync(remote_subject)
-    assert_equal 1, Subject.count
+    subject = create(:subject)
+    subject.expects(:sync_involved_users)
+    subject.sync(remote_subject)
   end
 
   test 'sync does not update involved users when the subject is not persisted' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:valid?).at_least_once.returns(false)
-    Subject.any_instance.expects(:sync_involved_users).never
-    Subject.sync(remote_subject)
+    subject = build(:subject)
+    subject.expects(:valid?).at_least_once.returns(false)
+    subject.expects(:sync_involved_users).never
+    subject.sync(remote_subject)
     assert_equal 0, Subject.count
   end
 
   test 'sync does not update involved users when there are no saved changes' do
     remote_subject = load_subject('subject_56.json')
-    Subject.sync(remote_subject)
-    Subject.any_instance.expects(:sync_involved_users).never
-    Subject.sync(remote_subject)
+    subject = create(:subject)
+    subject.sync(remote_subject)
+    subject.expects(:sync_involved_users).never
+    subject.sync(remote_subject)
   end
 
   test 'sync does not update involved users when there are no notifiable fields' do
     remote_subject = load_subject('subject_56.json')
-    Subject.any_instance.expects(:notifiable_fields).returns([])
-    Subject.any_instance.expects(:sync_involved_users).never
-    Subject.sync(remote_subject)
+    subject = create(:subject)
+    subject.expects(:notifiable_fields).returns([])
+    subject.expects(:sync_involved_users).never
+    subject.sync(remote_subject)
   end
 
   def load_subject(filename)
