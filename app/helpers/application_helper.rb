4a482824eddde37020b5ccf006e082a4d9a5fc1f
Andrew Nesbitt <andrewnez@gmail.com>
2022-01-04 17:10:41 +0000

    fix deprecation warning

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -54,6 +54,6 @@ module ApplicationHelper
 
   def svg(name)
     file_path = "#{Rails.root}/app/assets/images/#{name}.svg"
-    return File.read(file_path).html_safe if File.exists?(file_path)
+    return File.read(file_path).html_safe if File.exist?(file_path)
   end
 end
