15980dd04d2fd77d7e7286e005b0f83a9b28dbbf
Andrew Nesbitt <andrewnez@gmail.com>
2023-11-13 10:56:59 +0000

    upgrade to rails 7.1 defaults

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Octobox
     config.eager_load_paths << Rails.root.join("lib")
     config.exceptions_app = routes
 
-    config.load_defaults '6.1'
+    config.load_defaults '7.1'
 
     unless File.basename($0) == 'rake' || File.basename($0) == 'rails' || secrets.secret_key_base.length >= 32
       raise "SECRET_KEY_BASE should be a random key of at least 32 chars."
