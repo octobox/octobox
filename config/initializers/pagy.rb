9600664b279c375e56ee729f55e3d3a1997b67eb
Andrew Nesbitt <andrewnez@gmail.com>
2024-08-27 13:18:52 +0100

    Update config for pagy 9

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -1,5 +1,5 @@
 require 'pagy/extras/headers'
-Pagy::DEFAULT[:items] = 20
+Pagy::DEFAULT[:limit] = 20
 require 'pagy/extras/overflow'
 Pagy::DEFAULT[:overflow] = :last_page
 require 'pagy/extras/bootstrap'
